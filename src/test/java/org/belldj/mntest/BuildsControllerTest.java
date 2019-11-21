2f70ab74cf29ec66b02549a945c12aea7fbaa087
darren <djb.bell@gmail.com>
2019-11-21 01:21:13 +0000

    tests now use test containers

--- src/test/java/org/belldj/mntest/BuildsControllerTest.java
+++ src/test/java/org/belldj/mntest/BuildsControllerTest.java
@@ -1,33 +1,28 @@
 package org.belldj.mntest;
 
 import java.util.List;
-
 import javax.inject.Inject;
-
 import org.assertj.core.api.Assertions;
 import org.belldj.mntest.builds.RegisterBuildCommandT;
 import org.junit.jupiter.api.Test;
-
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.client.annotation.Client;
-import io.micronaut.test.annotation.MicronautTest;
 
-@MicronautTest
-class BuildsControllerTest {
+class BuildsControllerTest extends BaseContainerTest {
 
-	@Client(value = "/builds")
-	public interface BuildsClient {
-	    @Get(value = "/")
-	    List<RegisterBuildCommandT> findAll();
-	}
-	
-	@Inject
-	private BuildsClient client;
-	
-	@Test
-	void testFindAll() {
-		List<RegisterBuildCommandT> user = client.findAll();
-	    Assertions.assertThat(user).isNotNull();	
-	}
+  @Client(value = "/builds")
+  public interface BuildsClient {
+    @Get(value = "/")
+    List<RegisterBuildCommandT> findAll();
+  }
+
+  @Inject
+  private BuildsClient client;
+
+  @Test
+  void testFindAll() {
+    List<RegisterBuildCommandT> user = client.findAll();
+    Assertions.assertThat(user).isNotNull();
+  }
 
 }
--- src/test/java/org/belldj/mntest/GamesControllerWithDbTest.java
+++ src/test/java/org/belldj/mntest/GamesControllerWithDbTest.java
@@ -1,64 +1,13 @@
 package org.belldj.mntest;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.inject.Inject;
-
-import org.assertj.core.api.Assertions;
-import org.flywaydb.core.Flyway;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-import io.micronaut.test.annotation.MicronautTest;
-import io.micronaut.test.support.TestPropertyProvider;
-
-@Testcontainers
-@MicronautTest
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class GamesControllerWithDbTest implements TestPropertyProvider {
-
-  @Container
-  private static final PostgreSQLContainer<?> container =
-      new PostgreSQLContainer<>("postgres:11.5-alpine")
-          .withDatabaseName("test")
-          .withUsername("test")
-          .withPassword("");
-
-  @Inject
-  public Flyway flyway;
-
-  @AfterEach
-  public void afterEach() {
-    flyway.clean();
-  }
-
-  @BeforeEach
-  public void beforeEach() {
-    flyway.migrate();
-  }
+class GamesControllerWithDbTest extends BaseContainerTest {
 
   @Test
   public void testGamesController1() {
     assertThat(true).isTrue();
-  }
-
-  @Override
-  public Map<String, String> getProperties() {
-    System.out.println("### properties retieved");
-    HashMap<String, String> map = new HashMap<String, String>();
-    map.put("datasources.default.dialect", "POSTGRES");
-    map.put("datasources.default.url",             container.getJdbcUrl());
-    map.put("datasources.default.driverClassName", container.getDriverClassName());
-    map.put("datasources.default.username",        container.getUsername());
-    map.put("datasources.default.password",        container.getPassword());
-    return map;
   }
 
 }
